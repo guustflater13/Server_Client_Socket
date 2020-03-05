43a078f2897d618ef6ed0abeeab499c4c8c1ae72
Tjerk <tjerk.schuringa@gmail.com>
2020-03-05 20:33:16 +0100

    MVP of Server Client Socket test

---
+++ Socket_Client.py
@@ -0,0 +1,33 @@
+import socket
+import sys
+
+ip='192.168.178.7'
+port=6666
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = (ip, port)
+print('connecting to %s port %s' % server_address, file=sys.stderr)
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print('sending "%s"' % message, file=sys.stderr)
+    sock.sendto(message.encode(), (ip, port))
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(1024)
+        amount_received += len(data)
+        print('received "%s"' % data, file=sys.stderr)
+
+finally:
+    print('closing socket', file=sys.stderr)
+    sock.close()
